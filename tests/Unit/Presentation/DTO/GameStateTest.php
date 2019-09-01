df1488ada63749fd2b65360f287bd03f37ed0c23
f1r3starter <andrey.filenochko@gmail.com>
2019-09-01 22:17:51 +0300

    add DTO test

---
+++ tests/Unit/Presentation/DTO/GameStateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Unit\Presentation\DTO;
+
+use App\Domain\Entity\Board;
+use App\Domain\ValueObject\Sign;
+use App\Presentation\DTO\GameState;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class GameStateTest extends TestCase
+{
+    public function testSerialize(): void
+    {
+        $board = new Board(
+            new Sign(Sign::ZERO)
+        );
+
+        $gameState = new GameState($board);
+        $serializedState = $gameState->jsonSerialize();
+        
+        $this->assertEquals(
+            [
+                'boardState' => $board->getBoardState()->getState(),
+                'nextMove' => $board->getLastMove()->getOppositeSign(),
+                'isOver' => $board->isGameOver(),
+                'playerSign' => $board->getPlayerSign()->getValue(),
+                'winner' =>  $board->getWinner(),
+            ],
+            $serializedState);
+    }
+}
