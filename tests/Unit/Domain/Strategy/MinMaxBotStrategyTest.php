89e436e0ab7cefe3801ead48f46545d099abc18c
f1r3starter <andrey.filenochko@gmail.com>
2019-09-08 11:13:15 +0300

    bot strategy test

---
+++ tests/Unit/Domain/Strategy/MinMaxBotStrategyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Unit\Domain\Strategy;
+
+use App\Domain\Entity\Board;
+use App\Domain\Strategy\MinMaxBotStrategy;
+use App\Domain\ValueObject\Sign;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class MinMaxBotStrategyTest extends TestCase
+{
+    public function testMakeMove(): void
+    {
+        $board = new Board(
+            new Sign(Sign::CROSS)
+        );
+
+        $botStrategy = new MinMaxBotStrategy();
+        $botStrategySign = new Sign($board->getLastMove()->getOppositeSign());
+        $opponentBotStrategy = new MinMaxBotStrategy();
+        $opponentBotStrategySign = $board->getLastMove();
+
+        while (!$board->isGameOver()) {
+            $botStrategy->makeMove($board, $botStrategySign);
+            $opponentBotStrategy->makeMove($board, $opponentBotStrategySign);
+        }
+
+        $this->assertEquals($board->getWinner(), $botStrategySign);
+        $this->assertTrue($board->isGameOver());
+    }
+}
