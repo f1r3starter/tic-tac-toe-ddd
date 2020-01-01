3dd6184cb1de09a18f81abd52aeb0e279b53daa0
f1r3starter <andrey.filenochko@gmail.com>
2020-01-01 22:35:08 +0200

    remove jms

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,4 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
